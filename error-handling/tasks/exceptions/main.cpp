b300ff5465c208ce6827084fab79d700784a1dd9
SivaHub125 <sivathiagu02@gmail.com>
2024-06-13 22:31:44 +0530

    Solution for Error handling Assignment 1

--- error-handling/tasks/exceptions/main.cpp
+++ error-handling/tasks/exceptions/main.cpp
@@ -1,5 +1,14 @@
 #include "solution.h"
 
 int main() {
+    try {
+        double input;
+        std::cin>>input;
+        double result = computeSquareRoot(input);
+        std::cout << "Square root of " << input << " is: " << result << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+    return 0;
 }
 
--- error-handling/tasks/exceptions/solution.h
+++ error-handling/tasks/exceptions/solution.h
@@ -1,3 +1,10 @@
-#pragma once
-
 // Write your solution here
+#include <iostream>
+#include <cmath> // for sqrt
+
+double computeSquareRoot(double num) {
+    if (num < 0) {
+        throw std::domain_error("Negative input is not allowed.");
+    }
+    return std::sqrt(num);
+}
