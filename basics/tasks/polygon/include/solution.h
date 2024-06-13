8b5468a8c6b5f28da46b485c401f2f4b9d74f8d1
SivaHub125 <sivathiagu02@gmail.com>
2024-06-13 13:11:10 +0530

    Solution for Assignment 7

--- basics/tasks/polygon/include/solution.h
+++ basics/tasks/polygon/include/solution.h
@@ -1,3 +1,37 @@
-#pragma once
+#include<iostream>
+#include<memory>
 
 // Write your solution here
+
+class Polygon{
+public:
+    virtual double getArea() const = 0;
+};
+
+class Rectangle:public Polygon{
+private:
+    double length;
+    double width;
+public:
+    Rectangle(double a,double b){
+        length=a;
+        width=b;
+    }
+    double getArea() const override{
+        return length*width;
+    }
+};
+
+class RightTriangle:public Polygon{
+private:
+    double height;
+    double base;
+public:
+    RightTriangle(double a,double b){
+        height=a;
+        base=b;
+    }
+    double getArea() const override{
+        return 0.5*base*height;
+    }
+};
