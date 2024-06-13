344fa1b31d07278270720e251939130fc489bcba
SivaHub125 <sivathiagu02@gmail.com>
2024-06-13 09:00:30 +0530

    Solution for Assignment 1

--- basics/tasks/hello-world/include/hello_world.h
+++ basics/tasks/hello-world/include/hello_world.h
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 static void printHelloWorld() {
+    string name;
+    getline(cin,name,'\n');
+    cout<<"Hello, World!"<<endl;
+    cout<<name<<endl;
 }
