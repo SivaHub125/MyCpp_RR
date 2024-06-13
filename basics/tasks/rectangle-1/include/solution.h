43f1b5a6e7510e7af825f18f557e0d94a49e77b2
SivaHub125 <sivathiagu02@gmail.com>
2024-06-13 16:01:36 +0530

    Solution for Assignment 5

--- basics/tasks/rectangle-1/include/solution.h
+++ basics/tasks/rectangle-1/include/solution.h
@@ -1,5 +1,31 @@
-#pragma once
+#include<iostream>
 
 class Rectangle {
+private:
+    double width;
+    double height;
     // Write your code here
+public:
+    void setWidth(double num){
+        if(num<=0){
+            width=0;
+        }
+        else{
+            width = num;
+        }
+    }
+    void setHeight(double num){
+        if(num<=0){
+            height=0;
+        }
+        else{
+            height=num;
+        }
+    }
+    double getArea(){
+        return height*width;
+    }
+    double getPerimeter(){
+        return 2*(height+width);
+    }
 };
