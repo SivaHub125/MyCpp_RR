8ff9b8590929cdffffa092783810193c64dc4c23
SivaHub125 <sivathiagu02@gmail.com>
2024-06-13 16:20:27 +0530

    Solution for Assignment 6

--- basics/tasks/rectangle-2/include/solution.h
+++ basics/tasks/rectangle-2/include/solution.h
@@ -1,5 +1,45 @@
-#pragma once
+#include<iostream>
 
 class Rectangle {
+private:
+    double width;
+    double height;
     // Write your code here
+public:
+    Rectangle(){
+        std::cout<<"Rectangle created with default dimensions."<<std::endl;
+    }
+    Rectangle(double w,double h){
+        std::cout<<"Rectangle created with custom dimensions."<<std::endl;
+        height=h;
+        width=w;
+    }
+    ~Rectangle(){
+        std::cout<<"Deleting rectangle."<<std::endl;
+    }
+    void printDimensions(){
+        std::cout<<"Width: "<<width<<", Height: "<<height<<std::endl;
+    }
+    void setWidth(double num){
+        if(num<=0){
+            width=0;
+        }
+        else{
+            width = num;
+        }
+    }
+    void setHeight(double num){
+        if(num<=0){
+            height=0;
+        }
+        else{
+            height=num;
+        }
+    }
+    double getArea(){
+        return height*width;
+    }
+    double getPerimeter(){
+        return 2*(height+width);
+    }
 };
