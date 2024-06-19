a16410e1d93b58883b3361aa21e0ddc96e923992
SivaHub125 <sivathiagu02@gmail.com>
2024-06-19 08:53:04 +0530

    Solution for Error Handling assignment 2

--- templates/tasks/function/solution.h
+++ templates/tasks/function/solution.h
@@ -1,3 +1,29 @@
 #pragma once
-
+#include<iostream>
+using namespace std;
 // Write your solution here
+template <class T>
+T Max(T a,T b){
+    if(a>b){
+        return a;
+    }
+    else{
+        return b;
+    }
+}
+/*
+Point Max(Point a,Point b){
+    int dA=a.x*a.x+a.y*a.y;
+    int dB=b.x*b.x+b.y*b.y;
+    return (dA>dB)?a:b;
+}
+int Max(int a,int b){
+    return (a>b) ? a:b;
+}
+float Max(float a,float b){
+    return (a>b)?a:b;
+}
+char Max(char a,char b){
+    return (a>b)?a:b;
+}
+*/
