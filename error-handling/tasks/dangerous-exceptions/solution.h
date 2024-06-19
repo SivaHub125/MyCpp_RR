910b6c9bcd42eaf3025f7cdbb0bf4400dbab73af
SivaHub125 <sivathiagu02@gmail.com>
2024-06-19 22:55:30 +0530

    Solution for Error handling Assignment3

--- error-handling/tasks/dangerous-exceptions/solution.h
+++ error-handling/tasks/dangerous-exceptions/solution.h
@@ -3,6 +3,12 @@
 
 // Write your solution here
 void transferFunds(Account& from, Account& to, double amount) {
+    if(amount>10000){
+        throw std::runtime_error("Amount too large");
+    }
+    else{
     from.sub(amount);
     to.add(amount);
+    }
 }
+
