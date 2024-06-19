6e354563b477297fc0138cec9f6410e14829d6ab
SivaHub125 <sivathiagu02@gmail.com>
2024-06-19 09:38:35 +0530

    Solution for Template function Assignment 3

--- templates/tasks/specialization/solution.h
+++ templates/tasks/specialization/solution.h
@@ -1,6 +1,38 @@
 #pragma once
 
-// Write your solution here
+#include<iostream>
+
 struct AccessCounter {
     int count = 0;
+};
+
+template<class T>
+class Holder
+{
+    public:
+        T value;
+    Holder(T v){
+        value=v;
+    }
+    T getValue(){
+        return value;
+    }
+    void setValue(T v){
+        value=v;
+    }
+};
+template<>
+class Holder<AccessCounter>{
+    public:
+        AccessCounter counts;
+        Holder(AccessCounter v){
+            counts=v;
+        }
+        void setValue(AccessCounter v){
+            counts=v;
+        }
+        AccessCounter getValue(){
+            counts.count++;
+            return counts;
+        }
 };
